97819257c2953deef593435baaabcecfbb8b123c
marcoslora <marcoslora95@gmail.com>
2022-07-08 19:28:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# twitter-bot-51
+ Speed test and send the speed in Twitter
---
+++ main 2.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from passw import TWITTER_EMAIL, TWITTER_PASSWORD
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+CHROME_DRIVER_PATH = "/Users/ml/Documents/Development/chromedriver"
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        # accept_button = self.driver.find_element(By.ID, "_evidon-banner-acceptbutton")
+        # accept_button.click()
+        # time.sleep(3)
+
+        go_button = self.driver.find_element(By.CSS_SELECTOR, ".start-button a")
+        go_button.click()
+        time.sleep(60)
+        close_button = self.driver.find_element(By.XPATH,
+                                           "//*[@id='container']/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a")
+        close_button.click()
+        time.sleep(2)
+        self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+
+    def tweet_at_provider(self):
+        self.driver.get("https://twitter.com/login")
+        time.sleep(5)
+        email = self.driver.find_element(By.CSS_SELECTOR, ".r-fdjqy7")
+        email.click()
+        email.send_keys(TWITTER_EMAIL)
+        time.sleep(5)
+        email.send_keys(Keys.ENTER)
+        time.sleep(2)
+        password = self.driver.find_element(By.NAME, "password")
+        password.send_keys(TWITTER_PASSWORD)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+        time.sleep(5)
+        tweet_compose = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up? "
+        tweet_compose.click()
+        time.sleep(3)
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span')
+        tweet_button.click()
+        time.sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot.get_internet_speed()
+
+
+bot.tweet_at_provider()
